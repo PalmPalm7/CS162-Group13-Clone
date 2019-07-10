5f706ee2f0d04432643c0afeccee0d3f79d66dfd
jat1315 <31904697+jat1315@users.noreply.github.com>
2019-07-09 17:12:25 -0700

    modified include to include src

--- pintos/src/lib/kernel/list.c
+++ pintos/src/lib/kernel/list.c
@@ -1,6 +1,6 @@
 #include "list.h"
 #include "../debug.h"
-#include "thread/thread.c"
+#include "src/thread/thread.c"
 
 /* Our doubly linked lists have two header elements: the "head"
    just before the first element and the "tail" just after the
