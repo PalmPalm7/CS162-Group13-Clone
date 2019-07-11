6a945398390d3d9a43f6175523c7b621eb9c5d54
jat1315 <31904697+jat1315@users.noreply.github.com>
2019-07-10 19:46:52 -0700

    corrected typo in #include

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -11,7 +11,7 @@
 #include "threads/switch.h"
 #include "threads/synch.h"
 #include "threads/vaddr.h"
-#include "device/timer.h"
+#include "devices/timer.h"
 #ifdef USERPROG
 #include "userprog/process.h"
 #endif
