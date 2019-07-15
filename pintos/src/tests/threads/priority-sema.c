20b05c855de46c16bf14714431798c017073bd5b
Wenzheng Guo <guowz19@berkeley.edu>
2019-07-15 21:52:33 +0000

    pass all

--- pintos/src/tests/threads/priority-sema.c
+++ pintos/src/tests/threads/priority-sema.c
@@ -8,42 +8,37 @@
 #include "threads/synch.h"
 #include "threads/thread.h"
 #include "devices/timer.h"
-#include <stdlib.h>
-
 
 static thread_func priority_sema_thread;
 static struct semaphore sema;
 
 void
-test_priority_sema (void)
+test_priority_sema (void) 
 {
   int i;
-
+  
   /* This test does not work with the MLFQS. */
   ASSERT (!thread_mlfqs);
 
   sema_init (&sema, 0);
   thread_set_priority (PRI_MIN);
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 10; i++) 
     {
       int priority = PRI_DEFAULT - (i + 3) % 10 - 1;
       char name[16];
       snprintf (name, sizeof name, "priority %d", priority);
       thread_create (name, priority, priority_sema_thread, NULL);
     }
-  int ch;
-  for (i = 0; i < 10; i++)
+
+  for (i = 0; i < 10; i++) 
     {
-      
       sema_up (&sema);
-      msg ("Back in main thread.");
+      msg ("Back in main thread."); 
     }
-
-
 }
 
 static void
-priority_sema_thread (void *aux UNUSED)
+priority_sema_thread (void *aux UNUSED) 
 {
   sema_down (&sema);
   msg ("Thread %s woke up.", thread_name ());
