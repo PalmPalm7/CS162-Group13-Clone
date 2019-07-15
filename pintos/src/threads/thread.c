6adb5693f6d2d481bb6130dfc92fd3c4f300fcc8
Wenzheng Guo <guowz19@berkeley.edu>
2019-07-15 21:54:44 +0000

    pass all

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -545,6 +545,7 @@ int
 thread_get_recent_cpu (void)
 {
   return fix_round (fix_scale (thread_current ()->recent_cpu, 100));
+  
 }
 
 /* Idle thread.  Executes when no other thread is ready to run.
