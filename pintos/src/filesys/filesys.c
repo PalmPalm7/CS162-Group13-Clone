fc09028443b78209bb85327cc7b1a57e2936adfb
LiZuxin <lizx2019@berkeley.edu>
2019-08-09 23:02:49 +0000

    making commit for diffing

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -59,7 +59,7 @@ bool
 filesys_create (const char *name, off_t initial_size)
 {
   block_sector_t inode_sector = 0;
-  struct dir *dir = dir_open_current();
+  struct dir *dir = dir_open_root ();
   bool success = (dir != NULL
                   && free_map_allocate (1, &inode_sector)
                   && inode_create (inode_sector, initial_size)
@@ -79,7 +79,7 @@ filesys_create (const char *name, off_t initial_size)
 struct file *
 filesys_open (const char *name)
 {
-  struct dir *dir = dir_open_current ();
+  struct dir *dir = dir_open_root ();
   struct inode *inode = NULL;
 
   if (dir != NULL)
@@ -96,7 +96,7 @@ filesys_open (const char *name)
 bool
 filesys_remove (const char *name)
 {
-  struct dir *dir = dir_open_current ();
+  struct dir *dir = dir_open_root ();
   bool success = dir != NULL && dir_remove (dir, name);
   dir_close (dir);
 
